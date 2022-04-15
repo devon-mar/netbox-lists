c6da4b789bb3fe3072381c453432b723334b9efa
Devon Mar <devonm@mdmm.ca>
2022-04-15 00:58:14 -0700

    v2.0.1 bump

--- netbox_lists/__init__.py
+++ netbox_lists/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 from typing import List
 
@@ -8,7 +8,7 @@ from extras.plugins import PluginConfig
 class ListsPluginConfig(PluginConfig):
     name = "netbox_lists"
     verbose_name = "NetBox Lists"
-    version = "0.1.0"
+    version = __version__
     author = "Devon Mar"
     base_url = "lists"
     required_settings: List[str] = []
